140b237130191c830f422db05c013ddc965ad45d
Erwan <erwan.legloannec@outlook.com>
2022-11-29 11:53:01 +0100

    fix: project build

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!\n1 + 2 = {0}", add(1, 2));
 }
 
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
-}
-
-// This is a really bad adding function, its purpose is to fail in this
-// example.
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
 }
 
 #[cfg(test)]
@@ -21,12 +14,5 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(add(1, 2), 3);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
     }
 }
